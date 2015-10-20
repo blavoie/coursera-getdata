841ee342ef5bfd1a04ea516aeef8a773788fc8a9
Bruno Lavoie <bl@brunol.com>
2015-10-20 09:02:42 -0400

    Getting closer to the end. :)

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# coursera-getdata
-My work for Getting and Cleaning Data course project.
+# Course Project - Getting and Cleaning Data
+
+## Introduction
+
+This project contains one R script named `run_analysis.R`. 
+
+On the first run, it automatically downloads the required zip archive containing required data. After that, it loads the data and performs many operations until we have a tidy dataset that is more human consumable.
+
+As asked, the main steps performed are:
+ 
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+_In order to simplyfy the script, these steps aren't in this exact order. However, steps numbers are well indentified inside code._
+
+For a detailled rundown, just read the explicit script comments.
+
+## Data Information
+
+Data definitions are in [CookBook.md](CookBook.md) file.
+
+## Installing dependencies
+
+```{r}
+install.packate("dplyr")
+install.package("reshape2")
+```
--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,9 @@
 
+# Load dependeng libraries
+
+library(dplyr)
+library(reshape2)
+
 # Get de data if not exists.
 
 if(!file.exists("./data")) {
@@ -20,10 +25,10 @@ test.y   <-read.table("./data/UCI HAR Dataset/test/y_test.txt")
 train.subj <-read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 test.subj  <-read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-features <- read.table("./data/UCI HAR Dataset/features.txt")
-labels   <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+features   <- read.table("./data/UCI HAR Dataset/features.txt")
+activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
 
-# Merge training and test sets
+# Merges the training and the test sets to create one data set (#1)
 
 merged.x    <- rbind(train.x, test.x)
 merged.y    <- rbind(train.y, test.y)
@@ -32,4 +37,22 @@ merged.subj <- rbind(train.subj, test.subj)
 # Remove orignal datasets
 
 rm(train.x, test.x, train.y, test.y, train.subj, test.subj)
+
+# Appropriately labels the data set with descriptive variable names. (#4)
+# Also delete features frame once used to rename variables.
+
+names(merged.x)    <- as.character(features[, 2])
+names(merged.y)    <- "activity.id"
+names(merged.subj) <- "suject.id"
+names(activities)  <-  c("activity.id", "activity.name")
+
+rm(features)
+
+#  Extracts only the measurements on the mean and standard deviation for each measurement (#2)
+
+
+
+
+
+
 
